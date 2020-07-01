cc046369c2cdd055e053291a2fedd246f65f4bec
Hasan Polat <thatsnotpublic@gmail.com>
2020-07-01 15:17:27 +0300

    Update main.java

--- main.java
+++ main.java
@@ -5,7 +5,7 @@ public class main {
     
     public static void main(String[] args) {
         
-          thread1 t1 = new thread1();
+           thread1 t1 = new thread1();
            thread2 t2 = new thread2();
            
            t1.start();
